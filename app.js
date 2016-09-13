3f20c9165da265af9ae7047f1c7127bc2b641f3a
zodcikin <zeynep.odcikin@gmail.com>
2016-09-12 22:42:48 -0400

    initial commits

--- app.js
+++ app.js
@@ -2,44 +2,33 @@ var animals = ['Cat', 'Dog', 'Lama', 'Hampster'];
 
 $(document).on('click','.animal', function() { 
 	var animal = $(this).data("name"); 
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&limit=10&api_key=dc6zaTOxFJmzC";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+					animal + "&limit=10&api_key=dc6zaTOxFJmzC";
 		$.ajax({url: queryURL, method: 'GET'})	 
 	 		.done(function(response) {
 	 			var str = JSON.stringify(response, undefined, 2);
-	 			$('#animalPictures').empty();
-	 			
-            	for(var i=0;i<response.data.length;i++) {
+	 			$('#animalPictures').empty(); 	
+	 			for(var i=0;i<response.data.length;i++) {
             		var data = response.data[i];
-
             		var figure = $('<figure>');
   					var figCap = $('<figcaption>').text("Rating: " + data.rating);
 					var image = $('<img>').attr("src", data.images.original_still.url)
-										  .attr("width",100).attr("height",100);
-
-					figure.append(image).append(figCap);
+										  .addClass("img-rounded picture")
+										  .attr("width",200).attr("height",200)
+			    						  .attr('data-state', 'still');					  
+			    	image.attr('data-animate', data.images.original.url);
+			    	image.attr('data-still', data.images.original_still.url);
+                	figure.append(image).append(figCap);
 					$('#animalPictures').append(figure);
             	}	
 	 			
 	 	});
 });
 	
-function renderButtons() { 
 
-	$('#animalButtons').empty();
-	for(var i = 0; i < animals.length; i++) {
-		var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-	    a.addClass('animal'); // Added a class 
-		a.attr('data-name', animals[i]); // Added a data-attribute
-		a.text(animals[i]); // Provided the initial button text
-		$('#animalButtons').append(a); // Added the button to the HTML
-	}
-
-}
-
-// Then dynamicaly generates buttons for each movie in the array
 // ========================================================
 // This function handles events where one button is clicked
-$('#addAnimal').on('click', function(){
+$('#addAnimal').on('click', function() {
 	var name = $("#animal-input").val().trim().toLowerCase();
 	var animal = name.charAt(0).toUpperCase()+name.slice(1);
 	var notFound = $.inArray(animal, animals) == -1;
@@ -51,7 +40,31 @@ $('#addAnimal').on('click', function(){
 	return false;
 })
 
+$(document).on('click','.picture', function() {
+    var state = $(this).attr('data-state');
+    if( state == 'still') {
+        $(this).attr('src', $(this).data('animate'));
+        $(this).attr('data-state', 'animate');
+    } else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+});
+
 // ========================================================
 // This calls the renderButtons() function
-renderButtons();
+function renderButtons() {
+	$('#animalButtons').empty();
+	for(var i = 0; i < animals.length; i++) {
+		var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
+	    a.addClass('animal btn btn-primary'); // Added a class 
+		a.attr('data-name', animals[i]); // Added a data-attribute
+		a.text(animals[i]); // Provided the initial button text
+		$('#animalButtons').append(a); // Added the button to the HTML
+	}
+}
+
+$(document).ready(function() {
+	renderButtons();	
+});
 
